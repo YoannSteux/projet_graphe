073e3e8105810bd5e3ab95c5a75cb0d21e54e305
Steux Yoann <ysteux@localhost.localdomain>
2018-02-26 17:53:53 +0100

    ajout equals

--- files/projet.ml
+++ files/projet.ml
@@ -67,14 +67,59 @@ end
 
 (*Section 2*)
 
-let equals_aux g1 v1 g2 v2 = (true,[]);;
+let unmarked_aux sommet l_non_marques =
+   if (Mark.get sommet = 0) then
+      [sommet]@l_non_marques
+   else
+      l_non_marques
+;;
 
-let equals g1 v1 g2 v2 = (true,[]);;
+let unmarked l =
+  List.fold_right unmarked_aux l [] 
+;;
+
+let rec equals_aux g1 v1 g2 v2 = 
+  let s1 = unmarked (ordered_succ g1 v1)  
+    and s2 = unmarked (ordered_succ g2 v2)  
+  in
+  if ((s1=[]) && (s2=[])) then
+    (true, [])
+  else 
+    if ((s1=[]) || (s2=[])) then
+      (false, [])
+    else
+    
+      let (h1,h2) = (List.hd s1 , List.hd s2)
+      in
+      associate h1 h2;
+      let (bh,lh) = equals g1 h1 g2 h2
+        and
+          (bq,lq) = equals g1 v1 g2 v2
+      in
+      separate h1 h2;
+      let lf = lh@lq
+      in
+      ((bh && bq), [(h1,h2)]@lf)
+    
+and equals g1 v1 g2 v2 = 
+
+  associate v1 v2;
+  let (b, lr) = equals_aux g1 v1 g2 v2
+  in
+  let l = [(v1,v2)]@lr
+  in
+  separate v1 v2;
+  (b,l)
+
+;;
 
 
 (*Section 3*)
 
 let distance_aux g1 v1 g2 v2 = (0,[],[],[]);;
+
+
+
 
 let distance g1 v1 g2 v2 = (0,[],[],[]);;
 
